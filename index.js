cbba0878656239816c8200171a497561d3a3bd51
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-08 12:09:17 +0600

    JWT is added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express');
@@ -13,6 +14,28 @@ app.use(cors());
 app.use(express.json());
 
 
+//verifyJWT
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ massage: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(403).send({ massage: 'Forbidden access!!!', error : true });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+
+   
+
+}
+
+
 
 const uri = `mongodb+srv://${process.env.DB_INVENTORY}:${process.env.DB_PASS}@cluster0.rd1bs.mongodb.net/myFirstDatabase?retryWrites=true&`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,6 +46,15 @@ async function run() {
         await client.connect();
         const itemsColletion = client.db('inventory-management').collection('items');
 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // Items api
         app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemsColletion.find(query);
@@ -60,15 +92,26 @@ async function run() {
         });
 
         //My Items Api 
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = itemsColletion.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = itemsColletion.find(query);
+                const items = await cursor.toArray();
+                res.send(items);
+            }
+            else {
+                res.status(403).send({ massage: 'forbidden access' });
+            }
         });
         //DELETE API
-
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsColletion.deleteOne(query);
+            res.send(result);
+        });
 
     }
     finally {
